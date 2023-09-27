ad8a3b63926fa32fca845fc8fe533d93c1a3a315
Tolgaycode <tolgaycode@gmail.com>
2023-09-27 15:44:16 +0300

    re-orginaze

--- app.js
+++ app.js
@@ -10,37 +10,3 @@ document.getElementById("btn").addEventListener("click", function () {
 
   console.log(finalUrl);
 
-
-  import puppeteer from 'puppeteer';
-
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://www.google.com');
-  // other actions...
-  await browser.close();
-})();
-
-
-////////// Puppeteer //////////
-// const puppeteer = require("puppeteer");
-
-// try {
-//   (async () => {
-//     const browser = await puppeteer.launch();
-//     const page = await browser.newPage();
-//     await page.goto("https://learnwebcode.github.io/practice-requests/");
-
-//     const imgsrc = await page.evaluate(() => {
-//       return Array.from(document.querySelectorAll("img")).map(
-//         (img) => img.src
-//       );
-//     });
-
-//     console.log(imgsrc);
-
-//     await browser.close();
-//   })();
-// } catch (err) {
-//   console.error(err);
-// }
